74cd1e508029425773d67c67a708d1e7abc0a423
App Academy Student <aa@Apps-Mac-mini.local>
2019-03-26 16:57:00 -0400

    Prepare for initial Heroku push

---
+++ frontend/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // const store = configureStore();
+    const root = document.getElementById('root');
+    
+    ReactDOM.render(<h1>Welcome</h1>, root);
+});
