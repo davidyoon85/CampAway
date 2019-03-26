52454573535a9d537330ea7c4649be18e720359b
App Academy Student <aa@Apps-Mac-mini.local>
2019-03-26 17:59:48 -0400

    Complete Phase 1: User Auth

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // const store = configureStore();
+    const store = configureStore();
     const root = document.getElementById('root');
     
     ReactDOM.render(<h1>Welcome</h1>, root);
