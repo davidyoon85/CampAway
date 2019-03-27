db8abef56a6bd1c5c0411001b6d0524bdc0fab2c
App Academy Student <aa@Apps-Mac-mini.local>
2019-03-27 14:00:38 -0400

    Delete bundle.js

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { signup, login } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     
@@ -19,11 +18,6 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         store = configureStore();
     }
-
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-    window.signup = signup;
-    window.login = login;
 
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root);
