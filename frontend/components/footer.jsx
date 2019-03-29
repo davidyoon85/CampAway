12f3255f9057e5aaa740a2d4614bd6fa8baface2
App Academy Student <aa@Apps-Mac-mini-5.local>
2019-03-29 12:16:52 -0400

    add footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Footer = () => {
     return (
-    <div clasName="foot_div">
-        <img className="footer" id="footer_1" src={window.footerURL}/>
+    <div className="foot_div">
+        <div className="foot_description">
+            <div className="foot_description_title">
+                Hipcamp is everywhere you want to camp.
+            </div>
+            <div className="foot_description_text">
+                Discover unique experiences on ranches, nature preserves, farms,vineyards, and public campgrounds across the U.S. Book tent camping, treehouses, cabins, yurts, primitive backcountry sites,car camping, airstreams, tiny houses, RV camping, glamping tents and more.
+            </div>
+        </div>
+
+        <div className="foot_social">
+            <div className="foot_social_title">
+                Social
+            </div>
+            <div className="foot_social_text">
+                Discover unique experiences on ranches, nature preserves, farms,vineyards, and public campgrounds across the U.S. Book tent camping, treehouses, cabins, yurts, primitive backcountry sites,car camping, airstreams, tiny houses, RV camping, glamping tents and more.
+            </div>
+        </div>
     </div>
     )
 };
