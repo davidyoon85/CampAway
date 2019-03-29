f4ffe33309a4a626b9cbec6725953fbdfdd4fc02
App Academy Student <aa@Apps-Mac-mini-5.local>
2019-03-29 14:28:56 -0400

    bug fixes

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="foot_div">
         <div className="foot_description">
             <div className="foot_description_title">
-                Hipcamp is everywhere you want to camp.
+                Camp Away is everywhere you want to camp.
             </div>
             <div className="foot_description_text">
                 Discover unique experiences on ranches, nature preserves, farms,vineyards, and public campgrounds across the U.S. Book tent camping, treehouses, cabins, yurts, primitive backcountry sites,car camping, airstreams, tiny houses, RV camping, glamping tents and more.
@@ -15,9 +15,20 @@ const Footer = () => {
         <div className="foot_social">
             <div className="foot_social_title">
                 <ul>
-                    <li>
-                            <a href="https://github.com/davidyoon85"><i class="fab fa-github">Github</i></a>
+                    <li className="social_link_li">
+                            <a href="https://github.com/davidyoon85" className="social_link"><i className="fab fa-github"> Github</i></a>
                     </li>
+                    <li className="social_link_li">
+                            <a href="www.linkedin.com/in/davidyoon85" className="social_link"><i className="fab fa-linkedin"> Linkedin</i></a>
+                    </li>
+                    <li className="social_link_li">
+                            <a href="https://www.instagram.com/davidyoon85/" className="social_link"><i class="fab fa-instagram"> Instagram</i></a>
+                    </li>
+
+
+
+
+                        
                 </ul>
             </div>
             <div className="foot_social_text">
