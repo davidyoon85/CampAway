b6a76e35ab5db5f10d64d17e590735756fd55239
David Yoon <davidyoon85@gmail.com>
2019-04-08 15:50:35 -0400

    splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -12,8 +12,122 @@ const Splash = () => {
         <div className="splash_subtitle2">
         Book unique camping experiences on over <nobr className="bold">300,000</nobr> campsites, ranches, vineyards, public parks and more.
         </div>
-        <div className="spot_index_container">
-            <SpotSearchContainer />
+        <div className="splash_subtitle3">
+        Discover camping...
+        </div>
+
+        <div className="splash_main_section">
+
+            <div className="splash_section">
+                <div className="index_section">
+                    <img src="https://img.hipcamp.com/image/upload/q_auto,f_auto/v1544737872/best2018hipcamps_v6pluc.jpg" />
+                        <div className="index_section_container">    
+                            {/* <div className="index_section_header"> */}
+                            <h3 className="index_section_header">See the nominess</h3>
+                            {/* </div> */}
+                            <div className="index_section_subheader">
+                            2018 Hipcamps of the year
+                            </div>
+                        </div>
+                </div>
+
+                <div className="index_section">
+                    <img src="https://img.hipcamp.com/image/upload/f_auto,q_auto/v1528746350/01-book_arxtej.jpg" />
+                        <div className="index_section_container">   
+                            {/* <div className="index_section_header"> */}
+                            <h3 className="index_section_header">See the nominess</h3>
+                            {/* </div> */}
+                            <div className="index_section_subheader">
+                            2018 Hipcamps of the year
+                            </div>
+                        </div>    
+                </div>
+
+                <div className="index_section">
+                    <img src="https://img.hipcamp.com/image/upload/f_auto,q_auto/v1528746350/02-this_vkgeye.jpg" />
+                        <div className="index_section_container">   
+                            {/* <div className="index_section_header"> */}
+                            <h3 className="index_section_header">See the nominess</h3>
+                            {/* </div> */}
+                            <div className="index_section_subheader">
+                            2018 Hipcamps of the year
+                            </div>
+                        </div>
+                </div>
+
+                <div className="index_section">
+                    <img src="https://img.hipcamp.com/image/upload/q_auto,f_auto/v1544737872/best2018hipcamps_v6pluc.jpg" />
+                        <div className="index_section_container">  
+                            {/* <div className="index_section_header"> */}
+                            <h3 className="index_section_header">See the nominess</h3>
+                            {/* </div> */}
+                            <div className="index_section_subheader">
+                            2018 Hipcamps of the year
+                            </div>
+                        </div>
+                </div>
+
+                <div className="index_section">
+                    <img src="https://img.hipcamp.com/image/upload/f_auto,q_auto/v1528746350/01-book_arxtej.jpg" />
+                        <div className="index_section_container"> 
+                            {/* <div className="index_section_header"> */}
+                            <h3 className="index_section_header">See the nominess</h3>
+                            {/* </div> */}
+                            <div className="index_section_subheader">
+                            2018 Hipcamps of the year
+                            </div>
+                        </div>
+                </div>
+
+                <div className="index_section">
+                    <img src="https://img.hipcamp.com/image/upload/f_auto,q_auto/v1528746350/02-this_vkgeye.jpg" />
+                        <div className="index_section_container">    
+                            {/* <div className="index_section_header"> */}
+                            <h3 className="index_section_header">See the nominess</h3>
+                            {/* </div> */}
+                            <div className="index_section_subheader">
+                            2018 Hipcamps of the year
+                            </div>
+                        </div>
+                </div>
+                
+                <div className="index_section">
+                    <img src="https://img.hipcamp.com/image/upload/q_auto,f_auto/v1544737872/best2018hipcamps_v6pluc.jpg" />
+                        <div className="index_section_container">
+                            {/* <div className="index_section_header"> */}
+                            <h3 className="index_section_header">See the nominess</h3>
+                            {/* </div> */}
+                            <div className="index_section_subheader">
+                            2018 Hipcamps of the year
+                            </div>
+                        </div>
+                </div>
+
+                <div className="index_section">
+                    <img src="https://img.hipcamp.com/image/upload/f_auto,q_auto/v1528746350/01-book_arxtej.jpg" /> 
+                        <div className="index_section_container">    
+                            {/* <div className="index_section_header"> */}
+                            <h3 className="index_section_header">See the nominess</h3>
+                            {/* </div> */}
+                            <div className="index_section_subheader">
+                            2018 Hipcamps of the year
+                            </div>
+                        </div>
+                </div>
+
+                <div className="index_section">
+                    <img src="https://img.hipcamp.com/image/upload/f_auto,q_auto/v1528746350/02-this_vkgeye.jpg" />
+                        <div className="index_section_container">
+                            {/* <div className="index_section_header"> */}
+                            <h3 className="index_section_header">See the nominess</h3>
+                            {/* </div> */}
+                            <div className="index_section_subheader">
+                            2018 Hipcamps of the year
+                            </div>
+                        </div>
+                </div>
+
+            </div>
         </div>
     </div>
     </>
