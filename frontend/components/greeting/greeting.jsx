a3a1220ab60ebec1d9538d82198fa2228e216140
David Yoon <davidyoon85@gmail.com>
2019-04-28 09:14:27 -0400

    fixed dropdown

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -3,42 +3,46 @@ import { Link } from 'react-router-dom';
 import DropDown from '../nav/drop_down';
 
 class Greeting extends React.Component {
-    constructor(props) {
-      
+    constructor(props) {   
         super(props);
-        this.onclick = this.onclick.bind(this);
-        // let currentUser = props.currentUser;
-        // let logout = props.logout
-        // let openModal = props.openModal;
+        // this.onclick = this.onclick.bind(this);
+        // this.mouseOut = this.mouseOut.bind(this);
     }
 
-    onclick() {
-        return e => {
-            if (e.target === e.currentTarget) {
-            $("#drop_menu").toggleClass("show");
-            }
-        };
-    }
+    // onclick() {
+    //     return e => {
+    //         if (e.target === e.currentTarget) {
+    //         $("#drop_menu").addClass("show");
+    //         }
+    //     };
+    // }
+
+    // mouseOut() {
+    //     return e => {
+    //         if (e.target === e.currentTarget) {
+    //         $("#drop_menu").removeClass("show");
+    //         }
+    //     };
+    // }
 
     render() {
        
         return (
             <div>
-                {this.props.currentUser ? (
-                <div className="nav_logged_in">
-                    <div className="nav_logged_in_right">
-                        <img  onClick={this.onclick()} className="tent_logo" src={window.footerURL} />
-            
-                        <div id="drop_menu">
-                            <div className="menu_section">
-                            <Link className="nav_host" to={'/host'}>Host</Link>
-                            <button className="nav_logged_in_logout" onClick={this.props.logout}>Log out</button>
-                            </div>
-                        </div>
-            
+            {this.props.currentUser ? (
+                <div className="dropdown"  >
+                    <img className="tent_logo" src={window.footerURL} />
+        
+                    <div id="drop_menu">
+                        {/* <ul className="menu_section"> */}
+                            <li><Link className="nav_host" to={'/host'}>Host</Link></li>
+                            <li><a className="nav_host" onClick={this.props.logout}>Log out</a></li>
+                        {/* </ul> */}
                     </div>
                 </div>
+
                 ) : (
+
                     <nav>
                     <ul className="user_session">
                         <li><button onClick={() => this.props.openModal('signup')}>Sign up</button></li>
