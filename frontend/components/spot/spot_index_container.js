466a6d59bdceb705847570f4e7352c94428afc79
David Yoon <davidyoon85@gmail.com>
2019-05-13 20:54:13 -0400

    css fixes

--- frontend/components/spot/spot_index_container.js
+++ frontend/components/spot/spot_index_container.js
@@ -2,11 +2,14 @@ import { connect } from 'react-redux';
 import SpotIndex from './spot_index';
 import { fetchAllSpots, deleteSpot } from '../../actions/spot_actions';
 import { selectSpots } from '../../reducers/selectors';
+import { fetchReviews } from '../../actions/review_actions';
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state) => {
+    return {
     spots: selectSpots(state),
-    user: state.entities.users[state.session.id]
-});
+    user: state.entities.users[state.session.id],
+}
+};
 
 const mapDispatchToProps = (dispatch) => {
     
