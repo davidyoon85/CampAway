e25abef239538251a7d5d225bc6c1be0008cc173
David Yoon <davidyoon85@gmail.com>
2019-06-28 22:07:33 -0400

    try new york only

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -53,7 +53,7 @@ class Splash extends React.Component {
                 <div className="search_bar_main">
                     <i className="fas fa-search fa-lg search_bar_icon"></i>
                     <form className="search_form" onSubmit={this.handleSubmit}>
-                        <input placeholder="Try New York, Los Angeles, Seoul..." className="search_bar" onChange={this.update('searchParams')}
+                        <input placeholder="Try New York..." className="search_bar" onChange={this.update('searchParams')}
                             value={this.state['searchParams']}/>
                         <input onClick={() => this.handleSubmit} type="submit" className="search_button" value="Search" /> 
                     </form>
