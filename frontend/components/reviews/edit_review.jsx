427277c599d4c53d3bc30bdcad9d33714540c964
David Yoon <davidyoon85@gmail.com>
2020-02-23 15:06:11 -0500

    convert edit review to hooks

--- frontend/components/reviews/edit_review.jsx
+++ frontend/components/reviews/edit_review.jsx
@@ -1,70 +1,55 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-class EditReview extends Component {
-  constructor(props) {
-    super(props);
-    this.state = this.props.review;
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.goBack = this.goBack.bind(this);
-  }
+function EditReview(props) {
+  const [review, setReview] = useState(props.review);
 
-  componentDidMount() {
+  useEffect(() => {
     window.scrollTo(0, 0);
-    this.props.fetchReview(this.props.match.params.reviewId);
-    this.props.fetchSingleSpot(this.props.match.params.spotId);
-  }
+    props.fetchReview(props.match.params.reviewId);
+    props.fetchSingleSpot(props.match.params.spotId);
 
-  componentWillReceiveProps(newProps) {
-    if (this.props.review !== newProps.review) {
-      this.setState(newProps.review);
-    }
-  }
+    if (props.review) setReview(props.review)
+  }, [props.review])
 
-  update(field) {
+  function update() {
     return e => {
-      this.setState({ [field]: e.target.value });
+      setReview(e.target.value)
     };
   }
 
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    ;
-    this.props
-      .updateReview(this.props.match.params.spotId, this.state)
-      .then(() => this.goBack());
+
+    let payload = { 'description': description }
+    props.updateReview(props.match.params.spotId, payload)
+      .then(() => goBack());
   }
 
-  toDateInputValue() {
-    let date = new Date();
-    return date.toJSON().slice(0, 10);
+  function goBack() {
+    props.history.goBack();
   }
 
-  goBack() {
-    this.props.history.goBack();
-  }
+  return (
+    <div className="create_review_form">
+      <div className="review_spot_title">{props.spot.title}</div>
+      <form className="review_form" onSubmit={handleSubmit}>
+        <textarea
+          className="review_description"
+          cols="50"
+          rows="10"
+          maxLength="500"
+          value={state.description}
+          onChange={update()}
+        />
+        <input
+          className="create_review_submit"
+          type="submit"
+          value="Submit Review"
+        />
+      </form>
+    </div>
+  );
 
-  render() {
-    return (
-      <div className="create_review_form">
-        <div className="review_spot_title">{this.props.spot.title}</div>
-        <form className="review_form" onSubmit={this.handleSubmit}>
-          <textarea
-            className="review_description"
-            cols="50"
-            rows="10"
-            maxLength="500"
-            value={this.state.description}
-            onChange={this.update("description")}
-          />
-          <input
-            className="create_review_submit"
-            type="submit"
-            value="Submit Review"
-          />
-        </form>
-      </div>
-    );
-  }
 }
 
 export default EditReview;
