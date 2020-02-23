b99ec1791e188dbe68a4ded8b62434d348a5af62
David Yoon <davidyoon85@gmail.com>
2020-02-23 15:10:33 -0500

    convert reviews index to hooks

--- frontend/components/reviews/review_index.jsx
+++ frontend/components/reviews/review_index.jsx
@@ -1,38 +1,31 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 
 import ReviewIndexItem from "./review_index_item";
 
-class ReviewIndex extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
+function ReviewIndex(props) {
+  useEffect(() => {
     window.scrollTo(0, 0);
-    this.props.fetchReviews(this.props.match.params.spotId);
+    props.fetchReviews(props.match.params.spotId);
+  }, [])
+
+  const { reviews, deleteReview, currentUser } = props;
+
+  if (!reviews) {
+    return null;
   }
 
-  render() {
-    const { reviews, deleteReview, currentUser } = this.props;
+  const reviewsList = Object.values(reviews).map(review => {
+    return (
+      <ReviewIndexItem
+        key={review.id}
+        review={review}
+        deleteReview={deleteReview}
+        currentUser={currentUser}
+      />
+    );
+  });
 
-    if (!reviews) {
-      return null;
-    }
-    const spotId = this.props.match.params.spotId;
-
-    const reviewsList = Object.values(reviews).map(review => {
-      return (
-        <ReviewIndexItem
-          key={review.id}
-          review={review}
-          deleteReview={deleteReview}
-          currentUser={currentUser}
-        />
-      );
-    });
-
-    return <ul className="reviews_container">{reviewsList}</ul>;
-  }
+  return <ul className="reviews_container">{reviewsList}</ul>;
 }
 
 export default ReviewIndex;
