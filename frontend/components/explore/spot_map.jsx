56af0c59dd247763379b77cd98ec8013619ba835
David Yoon <davidyoon85@gmail.com>
2020-04-19 17:11:22 -0400

    convert spot_map.jsx to hooks

--- frontend/components/explore/spot_map.jsx
+++ frontend/components/explore/spot_map.jsx
@@ -1,54 +1,61 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import MarkerManager from "../../util/marker_manager";
 import { applyFilters } from "../../util/filter_util";
 
-class SpotMap extends Component {
-  constructor(props) {
-    super(props);
+function SpotMap({ spots, filters, geoLocation, updateFilter, receiveGeolocation }) {
+  let ref = useRef();
+  let geoCoder, map, markerManager;
 
-    this.geoCoder = new google.maps.Geocoder();
-    this.centerMapOnSearch = this.centerMapOnSearch.bind(this);
-    this.centerMap = this.centerMap.bind(this);
-    this.registerListeners = this.registerListeners.bind(this);
-  }
+  useEffect(() => {
 
-  componentDidMount() {
     let mapCenter = { lat: 40.751626, lng: -73.983926 };
+
     const mapOptions = {
       center: mapCenter,
       zoom: 11
     };
 
-    this.map = new google.maps.Map(this.mapNode, mapOptions);
-    this.MarkerManager = new MarkerManager(this.map);
-    this.MarkerManager.updateMarkers(this.props.spots);
-    this.registerListeners();
-  }
+    geoCoder = new google.maps.Geocoder();
+    map = new google.maps.Map(ref, mapOptions);
+    markerManager = new MarkerManager(map);
+    markerManager.updateMarkers(spots);
+    registerListeners();
+  }, [])
 
-  registerListeners() {
-    google.maps.event.addListener(this.map, "idle", () => {
-      const { north, south, east, west } = this.map.getBounds().toJSON();
+  useEffect(() => {
+    const filteredSpots = applyFilters(filters, spots);
+
+    if (markerManager && filteredSpots.length > 0) {
+
+      markerManager.updateMarkers(filteredSpots);
+      if (geoLocation.length > 0) centerMapOnSearch();
+    }
+  })
+
+  const registerListeners = () => {
+    google.maps.event.addListener(map, "idle", () => {
+      const { north, south, east, west } = map.getBounds().toJSON();
       let bounds = {
         northEast: { lat: north, long: east },
         southWest: { lat: south, long: west }
       };
 
-      this.props.updateFilter("location", bounds);
+      updateFilter("location", bounds);
     });
   }
 
-  centerMapOnSearch() {
-    this.geoCoder.geocode(
-      { address: this.props.geoLocation },
+  const centerMapOnSearch = () => {
+    geoCoder.geocode(
+      { address: geoLocation },
       (results, status) => {
         if (status === "OK") {
           if (results[0]) {
-            this.map.setZoom(12);
+            map.setZoom(12);
             let mapCenter = results[0].geometry.location;
-            this.map.setCenter(mapCenter);
-            const newBounds = this.map.getBounds();
-            this.map.fitBounds(newBounds);
-            this.props.receiveGeolocation(mapCenter);
+            map.setCenter(mapCenter);
+            const newBounds = map.getBounds();
+            map.fitBounds(newBounds);
+            receiveGeolocation(mapCenter);
           } else {
             return { lat: 40.751626, lng: -73.983926 };
           }
@@ -57,38 +64,11 @@ class SpotMap extends Component {
     );
   }
 
-  centerMap(callBack) {
-    this.geoCoder.geocode(
-      { address: this.props.geoLocation },
-      (results, status) => {
-        if (status === "OK") {
-          if (results[0]) {
-            let lat = results[0].geometry.location.lat();
-            let lng = results[0].geometry.location.lng();
-            let mapCenter = { lat, lng };
-            callBack(mapCenter);
-          } else {
-            mapCenter = { lat: 40.751626, lng: -73.983926 };
-            callBack(mapCenter);
-          }
-        }
-      }
-    );
-  }
-
-  componentDidUpdate() {
-    const filteredSpots = applyFilters(this.props.filters, this.props.spots);
-    this.MarkerManager.updateMarkers(filteredSpots);
-    if (this.props.geoLocation.length > 0) this.centerMapOnSearch();
-  }
-
-  render() {
-    return (
-      <div id="map-container">
-        <div className="map" ref={map => (this.mapNode = map)} />
-      </div>
-    );
-  }
+  return (
+    <div id="map-container">
+      <div className="map" ref={map => (ref = map)} />
+    </div>
+  );
 }
 
 export default SpotMap;
