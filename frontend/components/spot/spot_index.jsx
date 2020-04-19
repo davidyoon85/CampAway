c53aa177125cb5cfc91cbdbad2a442d5387d32cc
David Yoon <davidyoon85@gmail.com>
2020-04-19 16:47:13 -0400

    convert spot_index.jsx to hooks

--- frontend/components/spot/spot_index.jsx
+++ frontend/components/spot/spot_index.jsx
@@ -1,50 +1,37 @@
-import React, { Component } from "react";
-
+import React, { useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import { applyFilters } from "../../util/filter_util";
-
 import SpotIndexItem from "./spot_index_item";
-import Spinner from "../loader/spinner";
 
-class SpotIndex extends Component {
-  constructor(props) {
-    super(props);
+function SpotIndex({ spots, filters, history, user }) {
 
-    this.state = {
-      loading: true
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     window.scrollTo(0, 0);
-  }
+  }, [])
 
-  render() {
-    const { spots, filters } = this.props;
-    const filteredSpots = applyFilters(filters, spots);
+  const filteredSpots = applyFilters(filters, spots);
 
-    if (filteredSpots.length === 0) {
-      return (
-        <div className="spot_index">
-          <div className="spot_index_empty">
-            <h2> No spots match your current search. </h2>
-          </div>
+  if (filteredSpots.length === 0) {
+    return (
+      <div className="spot_index">
+        <div className="spot_index_empty">
+          <h2> No spots match your current search. </h2>
         </div>
-      );
-    } else {
-      return (
-        <div className="spot_index">
-          {filteredSpots.map(spot => (
-            <SpotIndexItem
-              push={this.props.history.push}
-              spot={spot}
-              key={spot.id}
-              user={this.props.user}
-            />
-          ))}
-        </div>
-      );
-    }
+      </div>
+    );
+  } else {
+    return (
+      <div className="spot_index">
+        {filteredSpots.map(spot => (
+          <SpotIndexItem
+            push={history.push}
+            spot={spot}
+            key={spot.id}
+            user={user}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
