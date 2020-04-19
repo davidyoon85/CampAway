0995135443d1cf77e33e39bce1771b248507fa65
David Yoon <davidyoon85@gmail.com>
2020-04-19 16:37:25 -0400

    convert photo_slidershow.jsx to hooks

--- frontend/components/spot/photo_slideshow.jsx
+++ frontend/components/spot/photo_slideshow.jsx
@@ -1,56 +1,50 @@
-import React, { Component } from "react";
-
+import React from "react";
 import Slider from "react-slick";
 
-class PhotoSlideshow extends Component {
-  constructor(props) {
-    super(props);
+function PhotoSlideshow({ spot }) {
+
+  const photos = [
+    "https://media.glampinghub.com/CACHE/images/accommodations/lakefront-log-cabin-rental-set-in-forestry-of-adirondack-park-new-1544021446318/c5e52bb1535628282aec8e7192952620.jpg"
+  ];
+
+  if (spot.photoUrls) {
+    photos = spot.photoUrls;
   }
 
-  render() {
-    let photos = [
-      "https://media.glampinghub.com/CACHE/images/accommodations/lakefront-log-cabin-rental-set-in-forestry-of-adirondack-park-new-1544021446318/c5e52bb1535628282aec8e7192952620.jpg"
-    ];
-
-    if (this.props.spot.photoUrls) {
-      photos = this.props.spot.photoUrls;
-    }
-
-    const settings = {
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      dots: false,
-      arrows: true,
-      infinite: true,
-      variableWidth: true,
-      className: "slider",
-      responsive: [
-        {
-          breakpoint: 700,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: false,
-            variableWidth: false
-          }
+  const settings = {
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    variableWidth: true,
+    className: "slider",
+    responsive: [
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: false,
+          variableWidth: false
         }
-      ]
-    };
+      }
+    ]
+  };
 
-    return (
-      <section className="spot_photo">
-        <Slider {...settings}>
-          {photos.map((photo, id) => {
-            return (
-              <div key={id}>
-                <img src={photo} alt="" />
-              </div>
-            );
-          })}
-        </Slider>
-      </section>
-    );
-  }
+  return (
+    <section className="spot_photo">
+      <Slider {...settings}>
+        {photos.map((photo, id) => {
+          return (
+            <div key={id}>
+              <img src={photo} alt="" />
+            </div>
+          );
+        })}
+      </Slider>
+    </section>
+  );
 }
 
 export default PhotoSlideshow;
